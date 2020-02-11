aad0965bc799de297ded95f8a7bac61a7b03bc92
Wenrui Zhang <wenrui.zhang@solace.com>
2020-02-11 11:27:58 -0500

    updated

--- src/app.js
+++ src/app.js
@@ -1,3 +1,58 @@
+import { HttpClient, json } from "aurelia-fetch-client";
+import { inject } from "aurelia-framework";
+
+@inject(HttpClient)
 export class App {
-  message = 'Hello World!';
+  
+  constructor(httpClient) {
+    this.btnStr = 'Request REBU Driver';
+    this.todos = [];
+    this.todoDescription = '';
+    this.httpClient = httpClient;
+    this.httpClient.configure(config => {
+      config
+        .withBaseUrl('https://pubsub.googleapis.com/v1/projects/stellar-arcadia-205014/subscriptions/')
+        .withDefaults({
+          credentials:'same-origin',
+          headers: {
+            'Authorization': 'Bearer ya29.Iq8BvQcvu4ymbxRaiyS_gxlFB23jgHS3al-2-hss1Ayijv7w3LltvVYE8mqd9Z2qY-tPkh1P3vdOGSAYpcxKCwuBatCjZr3pIHZfno5pVJ-rzYghACZwpkjM1Hl0hnCOR2ZhltBak33pM0vo9pIPNrw1M2mllEFrthPHuuMFWMM8we9qus00cZiIesufqi24O6BfYhaRj8jq3zpOh-G6y7BT4BOTVzhcjL6f0Y1HKzSJcA',
+            'Accept' : 'application/json',
+            'X-Requested-With' : 'Fetch'
+          }
+        })
+        .withInterceptor({
+          request(request) {
+            console.log(`Requesting ${request.method} ${request.url}`);
+            return request;
+          },
+          response(response) {
+            console.log(`Received ${response.status} ${response.url}`);
+            return response;
+          }
+        });
+    });
+  }
+
+  pullMessage(){
+    let message = {
+      "returnImmediately" : false,
+      "maxMessages": 10
+    };
+    this.httpClient
+      .fetch("acme-rideshare-driver-requested-subscription:pull",{
+        method: 'post',
+        body: json(message)
+      })
+      .then(response => {
+        let receivedMsg = response.json();
+        if(receivedMsg){
+          acknoledge(receivedMsg.ackId);
+          document.getElementById("buttons").style.bottom="10px";   
+        }
+      })
+  }
+
+  acknoledge(ackId){
+
+  }
 }
